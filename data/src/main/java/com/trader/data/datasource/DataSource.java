cd6a7819d80e3b9b7a6a3dfbd85c3612b704da3e
zdd <15068853770@163.com>
2025-11-12 16:48:27 +0800

    初步定义数据源接口

---
+++ data/src/main/java/com/trader/data/datasource/DataSource.java
@@ -0,0 +1,8 @@
+package com.trader.data.datasource;
+
+/**
+ * 数据同步源接口
+ * 用于定义通用的方法
+ */
+public interface DataSource {
+}
---
+++ data/src/main/java/com/trader/data/datasource/TuShareDataSource.java
@@ -0,0 +1,7 @@
+package com.trader.data.datasource;
+
+/**
+ * 通过TuShare数据源获取行情数据
+ */
+public class TuShareDataSource implements DataSource {
+}
