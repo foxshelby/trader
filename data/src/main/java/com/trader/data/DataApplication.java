7f24be885ed33154208e61e7209fcdd614793061
zdd <15068853770@163.com>
2025-11-17 11:24:56 +0800

    环境配置提交

--- data/src/main/java/com/trader/data/DataApplication.java
+++ data/src/main/java/com/trader/data/DataApplication.java
@@ -1,13 +1,24 @@
 package com.trader.data;
 
+import com.trader.data.service.PointInfluxDBService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class DataApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DataApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(DataApplication.class, args);
+
+        PointInfluxDBService bean = run.getBean(PointInfluxDBService.class);
+        Map<String, String> tags = new HashMap<>();
+        tags.put("trader", "trader");
+         bean.writePoint("temperature", tags, null, Instant.now());
     }
 
 }
--- data/src/main/java/com/trader/data/service/PointInfluxDBService.java
+++ data/src/main/java/com/trader/data/service/PointInfluxDBService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.util.Map;
 
-@Service
+//@Service
 public class PointInfluxDBService {
 
     private final WriteApiBlocking writeApi;
